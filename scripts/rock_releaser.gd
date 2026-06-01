88de9dab875e237f259d71043f778625bc904441
Simonandadog <simonspry09@gmail.com>
2026-06-01 12:55:34 +0200

    distance based bullet culling and rock dropping both functional

--- scripts/rock_releaser.gd
+++ scripts/rock_releaser.gd
@@ -5,8 +5,5 @@ extends Node2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print("rock blocker removed")
-	$StaticBody2D/CollisionShape2D.queue_free()
-	$StaticBody2D.queue_free()
-	$Area2D/CollisionShape2D.queue_free()
-	$Area2D.queue_free()
+	print("next stage activated")
 	queue_free()
