bf5f0442f07b599b3b7569652875328df01a0e6b
Simonandadog <simonspry09@gmail.com>
2026-06-01 16:47:33 +0200

    tried changing stuff. never try changing stuff

--- scripts/rock_releaser.gd
+++ scripts/rock_releaser.gd
@@ -6,4 +6,6 @@ extends Node2D
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print("rock blocker removed")
 	print("next stage activated")
-	queue_free()
+	for child in get_children():
+		if child is RigidBody2D:
+			child.sleeping = false
