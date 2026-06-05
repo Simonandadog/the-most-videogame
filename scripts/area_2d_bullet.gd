ad9e2a92731db962b466f11cf0a3f68d6daaae92
Simon Spry <simonspry09@gmail.com>
2026-06-05 08:25:57 +0200

    rework bullet removal tecnique to remove bug

--- scripts/area_2d_bullet.gd
+++ scripts/area_2d_bullet.gd
@@ -1,18 +1,27 @@
 extends Area2D
 
 var speed = 30000.0  
-var startpos = 0
+var startpos = Vector2(0,0)
 
 
-func _on_ready() -> void:
+func _ready() -> void:
 	startpos = self.global_position
+	
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	startpos - self.global_position
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
+	print("bullet removed")
+
 
 
 func _physics_process(delta):
 	position += transform.y * speed * delta * -1
-	if global_position.distance_to(startpos) > 40000:
-		queue_free()
-		print("bullet removed")
+#	if global_position.distance_to(startpos) > 40000:
+#		queue_free()
+#		print("bullet removed")
 
 
 
@@ -20,6 +29,6 @@ func _on_area_entered(area: Area2D) -> void:
 	area.queue_free()
 	queue_free()
 	
-func _on_body_entered(body: Node2D) -> void:
-	body.queue_free()
-	queue_free()
+#func _on_body_entered(body: Node2D) -> void:
+#	body.queue_free()
+#	queue_free()
