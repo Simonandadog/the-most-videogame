71311d9686fb93a775a5c3379f87db8ab4da0e47
Simonandadog <simonspry09@gmail.com>
2026-06-10 16:18:16 +0200

    final commit

--- scripts/area_2d_bullet.gd
+++ scripts/area_2d_bullet.gd
@@ -1,4 +1,5 @@
 extends Area2D
+@onready var explosion: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 var speed = 30000.0  
 
@@ -11,9 +12,11 @@ func _physics_process(delta):
 	position += transform.y * speed * delta * -1
 
 func _on_area_entered(area: Area2D) -> void:
+	explosion.play()
 	area.queue_free()
 	queue_free()
 	
 func _on_body_entered(body: Node2D) -> void:
+	explosion.play()
 	body.queue_free()
 	queue_free()
